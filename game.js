e029827faa0af5cd8b2f361fa8bbfe01157127eb
Debak Roy <debakroy7@gmail.com>
2023-04-07 13:44:08 +0530

    made the UI

--- game.js
+++ game.js
@@ -1,71 +1,92 @@
+const cChoice = document.querySelector('.computer .show-choice');
+const pChoice = document.querySelector('.player .show-choice');
+const pScore = document.querySelector('.player-score');
+const cScore = document.querySelector('.computer-score');
+const startBtn = document.querySelector('.start-game');
+const gameBody = document.querySelector('.game-body');
+const choiceButtons = document.querySelectorAll('.choices button');
+const cShowChoice = document.querySelector('.computer .show-choice');
+const pShowChoice = document.querySelector('.player .show-choice');
+const whoWon = document.querySelector('.who-won');
+let pWin = 0, cWin = 0;
+
+startBtn.addEventListener('click', () => {
+    gameBody.style.visibility = 'visible';
+    gameBody.style.top = '0';
+});
+
 function getComputerChoice() {
-    let choice = ["rock", "paper", "scissor"];
-    let getRndIndex = Math.floor(Math.random()*30)%3;
-    return choice[getRndIndex];
+    const choices = ['rock', 'paper', 'scissor'];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
 }
 
 function playRound(playerSelection, computerSelection) {
-    let playerWin=false,tie=false;
-
-    if(playerSelection === "rock" && computerSelection === "scissor") {
-        playerWin=true;
+    if (playerSelection === computerSelection) {
+        return 'tie';
     }
-    else if(playerSelection === "paper" && computerSelection === "rock") {
-        playerWin=true;
-    }
-    else if(playerSelection === "scissor" && computerSelection === "paper") {
-        playerWin=true;
-    }
-    else if(playerSelection === computerSelection) {
-        tie=true;
-    }
-
-    if(playerWin) {
-        return "player";
-    }
-    else if(tie) {
-        return "tie";
+    else if (
+        (playerSelection === 'rock' && computerSelection === 'scissor') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissor' && computerSelection === 'paper')
+    ) {
+        return 'player';
     }
     else {
-        return "computer";
+        return 'computer';
     }
 }
 
 function game() {
-    console.log("Let the game begin...");
+    choiceButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const playerSelection = button.value;
+            const computerSelection = getComputerChoice();
+            const result = playRound(playerSelection, computerSelection);
+            // console.log(`Player Chose: ${playerSelection}, Computer Chose: ${computerSelection}`);
+            cChoice.innerHTML = `<img width=250px height=250px alt="an image of rock" src="images/${computerSelection}.jpeg">`;
+            pChoice.innerHTML = `<img width=250px height=250px alt="an image of rock" src="images/${playerSelection}.jpeg">`;
+            if (result === 'player') {
+                pWin++;
+                pScore.innerHTML = `Player Score- ${pWin}`;
+                pShowChoice.style.borderColor = '#43eb34';
+                cShowChoice.style.borderColor = '#eb3434';
+                // console.log('Player won the round!');
+            }
+            else if (result === 'computer') {
+                cWin++;
+                cScore.innerHTML = `Computer Score- ${cWin}`;
+                cShowChoice.style.borderColor = '#43eb34';
+                pShowChoice.style.borderColor = '#eb3434';
+                // console.log('Computer won the round!');
+            }
+            else {
+                // console.log('It\'s a tie!');
+                cShowChoice.style.borderColor = '#5d5c5c';
+                pShowChoice.style.borderColor = '#5d5c5c';
+            }
+            // console.log(`Player wins: ${pWin}, Computer wins: ${cWin}`);
+            if(pWin==5 || cWin==5) {
+                checkWhoWon();
+            }
+        });
+    });
+}
 
-    let pWin=0, cWin=0;
-
-    for(let i=1;i<=5;i++) {
-        let playerSelection = prompt("Enter rock, paper or scissor:").toLowerCase();
-        let computerSelection = getComputerChoice();
-        let status = playRound(playerSelection,computerSelection);
-        if(status === "player") {
-            console.log("Player won the round!");
-            pWin++;
-        }
-        else if(status === "computer") {
-            console.log("Computer won the round!");
-            cWin++;
-        }
-        else {
-            console.log("It's a tie!");
-        }
-    }
-
-    if(pWin > cWin) {
-        alert("Player WON");
-        console.log("");
-        console.log("Player WON");
-    }
-    else if(pWin == cWin) {
-        alert("TIE");
-        console.log("");
-        console.log("TIE");
+function checkWhoWon() {
+    gameBody.style.top = '100vh';
+    startBtn.style.visibility = 'hidden';
+    whoWon.style.visibility = 'visible';
+    if (pWin > cWin) {
+        console.log('Player WON');
+        whoWon.innerHTML = `Player Won!🥳`;
+        // alert('Player WON');
     }
     else {
-        alert("Computer WON");
-        console.log("");
-        console.log("Computer WON");
+        console.log('Computer WON');
+        whoWon.innerHTML = `Computer Won!🤖`;
+        // alert('Computer WON');
     }
 }
+
+game();
